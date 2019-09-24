6540d181a5c803d76c9dee8278305070356d220b
Dylan DeGrood <degrood21@up.edu>
2019-09-23 17:09:26 -0700

    Update json.test.js

--- hw3/tests/json.test.js
+++ hw3/tests/json.test.js
@@ -6,12 +6,12 @@ const router = require("../routes/orders");
 
 describe("Example Router", () => {
 
-  it("should return 'hello world' for GET /example", () => {
+  it("matches if cherry quantity is 20 form JSON object", () => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
-            assert(myArr[0].quantity, "0");
+            assert(myArr[0].quantity, "20");
         }
     };
     xmlhttp.open("POST", "/orders", true);
